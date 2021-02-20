73b6ce4b9bd41a197cc314ff8b5ab27d71865d96
Lucas Parry <lucas.parry@ferocia.com.au>
2021-02-20 13:53:27 +1100

    Update state as we go

--- lib/sensibo/device.rb
+++ lib/sensibo/device.rb
@@ -8,32 +8,52 @@ class Sensibo
       store_state(metadata)
     end
 
+    def on?
+      metadata.fetch(:ac_state).fetch(:on)
+    end
+
     def state
-      get("pods/#{id}/acStates", { limit: 1 }).first[:ac_state].without(:timestamp)
+      get("pods/#{id}/acStates", { limit: 1 })
+        .first
+        .fetch(:ac_state)
+        .without(:timestamp)
+        .tap { |x| metadata[:ac_state] = metadata.fetch(:ac_state).merge(x) }
     end
 
     def switch_off
-      patch("pods/#{id}/acStates/on", {}, { new_value: false })
+      patch("pods/#{id}/acStates/on", {}, { new_value: false }).tap do |x|
+        metadata[:ac_state] = metadata.fetch(:ac_state).merge(x.fetch(:ac_state))
+      end
     end
 
     def switch_on
-      patch("pods/#{id}/acStates/on", {}, { new_value: true })
+      patch("pods/#{id}/acStates/on", {}, { new_value: true }).tap do |x|
+        metadata[:ac_state] = metadata.fetch(:ac_state).merge(x.fetch(:ac_state))
+      end
     end
 
     def climate_react_on
-      put("pods/#{id}/smartmode", {}, { enabled: true })
+      put("pods/#{id}/smartmode", {}, { enabled: true }).tap do |x|
+        metadata[:smart_mode] = metadata.fetch(:smart_mode).merge(x)
+      end
     end
 
     def climate_react_off
-      put("pods/#{id}/smartmode", {}, { enabled: false })
+      put("pods/#{id}/smartmode", {}, { enabled: false }).tap do |x|
+        metadata[:smart_mode] = metadata.fetch(:smart_mode).merge(x)
+      end
     end
 
     def climate_react_settings
-      get("pods/#{id}/smartmode")
+      get("pods/#{id}/smartmode").tap do |x|
+        metadata[:smart_mode] = metadata.fetch(:smart_mode).merge(x)
+      end
     end
 
     def climate_react_update_settings(args)
-      post("pods/#{id}/smartmode", {}, climate_react_settings.merge(args))
+      post("pods/#{id}/smartmode", {}, climate_react_settings.merge(args)).tap do |x|
+        metadata[:smart_mode] = metadata.fetch(:smart_mode).merge(x)
+      end
     end
 
     def refresh
@@ -42,10 +62,13 @@ class Sensibo
 
     private
 
+    attr_reader :metadata
+
     def store_state(metadata)
       @id = metadata.fetch(:id)
       @name = metadata.fetch(:room).fetch(:name)
       @metadata = metadata
+      @metadata = metadata.slice(:ac_state, :measurements, :smart_mode) if !ENV['DEBUG']
     end
   end
 end
